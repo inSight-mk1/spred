4fe2765164032ff0217a9bfd2944992b6e06e308
dongwei.fei <zjfdw@outlook.com>
2020-05-29 17:29:14 +0800

    finish rps choice

--- choices/rps_choices.py
+++ choices/rps_choices.py
@@ -1,9 +1,11 @@
 from indicators.rps import RPS
 from base.pool.listed_time_pool import ListedTimePool
+import numpy as np
 
 
 class RPSChoices(object):
     def __init__(self, rps_n=50, thresh=87.0, new_high_t=20):
+        self.thresh = thresh
         # 1. get stock pool
         self.stock_pool = ListedTimePool().pool
 
@@ -14,5 +16,21 @@ class RPSChoices(object):
         self.selected_pool = self.select()
 
     def select(self):
-        self.indicators.rps_array
+        rps_array = self.indicators.rps_array
+        rps_max = np.max(rps_array, axis=1)
+        current_max = (rps_max == rps_array[:, 0])
+        rps_good = (rps_max >= self.thresh)
+        # print(current_max.shape, rps_good.shape)
+        selected_map = np.logical_and(current_max, rps_good)
+        # print(selected_map)
+        selected_pool = []
+        for i, stock in enumerate(self.stock_pool):
+            if selected_map[i]:
+                selected_pool.append(stock)
+                print(stock['ts_code'][0], 'rps', rps_array[i][0])
+        return selected_pool
 
+
+if __name__ == '__main__':
+    choices = RPSChoices()
+    print(len(choices.selected_pool))
--- get_data.py
+++ get_data.py
@@ -12,7 +12,7 @@ ts.set_token(my_token)
 save_path = cfgp.save_path
 
 start_date = '20100101'
-end_date = '20200527'
+end_date = '20200528'
 min_len = 500
 
 max_every_min = 200.0
